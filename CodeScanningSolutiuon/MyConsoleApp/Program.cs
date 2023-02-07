753b958e1020da28460e9cbf77e58466124c16c9
Bas van de Sande <bvandesande@xpirit.com>
2023-02-07 18:32:33 +0100

    initial version

---
+++ CodeScanningSolutiuon/MyConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MyConsoleApp
+{
+    public class Program
+    {
+        // force warning in codeQL
+        public static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+
+            Console.ReadKey();
+        }
+    }
+}
